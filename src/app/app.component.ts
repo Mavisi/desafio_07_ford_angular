02e6905b5423a7ff5652f7986ab53fb7379ab3ad
Marcos Santana <marcos.vssjr@outlook.com>
2025-05-06 15:06:46 -0300

    evoluindo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./pages/login/login.component";
-import { HomeComponent } from "./pages/home/home.component";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginComponent, HomeComponent, DashboardComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,16 +1,22 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule],
+  standalone: true, 
+  imports: [
+    FormsModule,    
+    CommonModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   usuario: string = '';
   senha: string = '';
+loginForm: any;
 
   constructor(private http: HttpClient) {}
 
