aa6ef12c906a6de454e9779302bdddf9025c317e
Marcos santana <marcos.vssjr@outlook.com>
2025-05-07 00:14:18 -0300

    home pronto

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
 
+  logout() {
+    localStorage.removeItem('usuario'); // Remove dados de login
+    this.router.navigate(['/']);        // Redireciona para login
+  }
 }
