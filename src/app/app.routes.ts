f2b258bf1a11de42912177c8a54e090c718b134d
Marcos santana <marcos.vssjr@outlook.com>
2025-05-06 21:38:50 -0300

    tela de home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
+    { path: '', component: LoginComponent },
     { path: 'home', component: HomeComponent },
     { path: 'dashboard', component: DashboardComponent },
 
