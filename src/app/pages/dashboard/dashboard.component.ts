35f503ea23691c2ad3a103f5004aa8bedffba107
Marcos santana <marcos.vssjr@outlook.com>
2025-05-07 10:56:51 -0300

    começando o dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule,FormsModule,CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  veiculos = ['Ranger', 'Mustang', 'Territory', 'Bronco Sport'];
+  veiculoSelecionado = 'Mustang';
 
+  constructor(private router: Router) {}
+
+  logout() {
+    localStorage.removeItem('usuario'); // Remove dados de login
+    this.router.navigate(['/']);        // Redireciona para login
+  }
+  
 
   
 }
